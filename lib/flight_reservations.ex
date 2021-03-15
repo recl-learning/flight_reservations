9feae9b8b735c44ab55b5b81ad52a8a37ce7354e
Ricardo Ebbers <recl@cin.ufpe.br>
2021-03-15 16:56:03 -0300

    Started project

---
+++ lib/flight_reservations.ex
@@ -0,0 +1,18 @@
+defmodule FlightReservations do
+  @moduledoc """
+  Documentation for `FlightReservations`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FlightReservations.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/flight_reservations_test.exs
@@ -0,0 +1,8 @@
+defmodule FlightReservationsTest do
+  use ExUnit.Case
+  doctest FlightReservations
+
+  test "greets the world" do
+    assert FlightReservations.hello() == :world
+  end
+end
